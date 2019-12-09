1bdbabad1d8498bc0fe57b416a066e7226c03e46
Jane Madonna <janecogger@gmail.com>
2019-12-09 12:05:42 -0500

    started skeletons

---
+++ client/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Home extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            stars: []
+        }
+    }
+}
+
+export default Home
