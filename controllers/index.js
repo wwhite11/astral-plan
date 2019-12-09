1b993b8472133d9a6d49649d3546120db1c76b98
jeremyrrose <jeremy.r.rose@gmail.com>
2019-12-09 12:36:39 -0500

    api endpoints for user signup and sign-in

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,64 @@
 const { User, Star, Planet, Moon } = require('../models');
 const sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const SALT_ROUNDS = 11;
+const TOKEN_KEY = '0fbfec5e1c6701506ab3f8a3162990ba';
+
+const signup = async (req, res) => {
+	try {
+		console.log(req.body)
+		const { username, firstName, lastName, email, password } = req.body
+		const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
+		const user = await User.create({
+			username,
+            password_digest,
+            firstName,
+            lastName,
+            email
+		})
+		const payload = {
+			id: user.id,
+			username: user.username,
+			email: user.email
+		}
+
+		const token = jwt.sign(payload, TOKEN_KEY)
+		return res.status(201).json({ user, token })
+	} catch (error) {
+		console.log(
+			'You made it to the signup controller, but there was an error :('
+		)
+		return res.status(400).json({ error: error.message })
+	}
+}
+
+const signIn = async (req, res) => {
+	try {
+		console.log(req.body);
+		const { username, password } = req.body;
+		const user = await User.findOne({
+			where: {
+				username
+			}
+		})
+		if (await bcrypt.compare(password, user.dataValues.password_digest)) {
+			const payload = {
+				id: user.id,
+				username: user.username,
+				email: user.email
+			}
+
+			const token = jwt.sign(payload, TOKEN_KEY)
+			return res.status(201).json({ user, token })
+		} else {
+			res.status(401).send('Username and password do not match.')
+		}
+	} catch (error) {
+		return res.status(500).json({ error: error.message })
+	}
+}
 
 const getUser = async (req, res) => {
     try {
@@ -65,6 +123,8 @@ const getStar = async (req, res) => {
 }
 
 module.exports = {
+    signup,
+    signIn,
     getUser,
     getStar,
     getAllStars
