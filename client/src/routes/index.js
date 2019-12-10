56ae6f11f85d7aa81cd66a665b80c7b06c8c0a29
Jane Madonna <janecogger@gmail.com>
2019-12-10 10:18:34 -0500

    finished routing

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,8 +5,12 @@ import Dashboard from '../screens/Dashboard'
 import SignUp from '../screens/SignUp'
 import SignIn from '../components/shared/SignIn'
 import CreateSystem from '../screens/CreateSystem'
+import UpdateSystem from '../screens/UpdateSystem'
+import Stars from '../screens/Stars'
+import Star from '../screens/Star'
+import SignOut from '../components/shared/SignOut'
 
-const Routes = ({ user, setUser, createSystem }) => (
+const Routes = ({ user, setUser, clearUser, createSystem, updateSystem }) => (
     <Switch>
         <Route
         exact
@@ -14,16 +18,33 @@ const Routes = ({ user, setUser, createSystem }) => (
         render={props => (user ? <Dashboard /> : <Home {...props} />)}
         />
         <Route
-        path="/signup"
+        path="/sign-up"
         render={props => <SignUp {...props} setUser={setUser} />}
         />
         <Route
-        path="/signin"
+        path="/sign-in"
         render={props => <SignIn {...props} setUser={setUser} />}
+        />
+        <Route
+        exact
+        path="/sign-out"
+        render={props => <SignOut {...props} clearUser={clearUser} user={user} />}
+        />
+        <Route 
+        path="/stars"
+        render={props => <Stars {...props} />}
+        />
+        <Route 
+        path="/stars/:id"
+        render={props => <Star {...props} />}
         />
         <Route 
         path="/create-system"
         render={props => <CreateSystem {...props} createSystem={createSystem} />}
+        />
+        <Route 
+        path="/update-system/:id"
+        render={props => <UpdateSystem {...props} updateSystem={updateSystem} />}
         />
     </Switch>
 )
---
+++ client/src/screens/Star.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Systems from '../components/shared/Systems'
+
+const Star = (props) => {
+    return (
+        <div>
+            <Systems />
+        </div>
+    )
+}
+
+export default Star
