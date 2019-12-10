bf94191ae37e90cf3b9ee3e352713323d0398a1f
jeremyrrose <jeremy.r.rose@gmail.com>
2019-12-10 09:31:34 -0500

    added client JWT to Axios headers, export TOKEN_KEY from controllers to new helpers/index.js

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -18,6 +18,7 @@ if (window.location.hostname === 'localhost') {
 const api = Axios.create({
   baseURL: apiUrl,
   headers: {
+		Authorization: `Bearer ${JwtToken}`,
     'Access-Control-Allow-Origin': '*'
   }
 })
