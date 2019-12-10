6461703048430aca443a37a56e4631d8abb44443
Winston94 <2winstonrwhite@gmail.com>
2019-12-10 11:52:58 -0500

    almost there with the modal

--- client/src/screens/CreateSystem.jsx
+++ client/src/screens/CreateSystem.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import PromptModal from '../components/shared/PromptModal'
 
-const CreateSystem = () => {
-    return (
-        <div>Create System Component</div>
-    )
+class CreateSystem extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            name: '',
+            size: '',
+            color: '',
+        }
+    }
+    toggleModal = () => {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+    
+      render() {
+        return (
+          <div className="App">
+            <button onClick={this.toggleModal}>
+              Create
+            </button>
+    
+            <PromptModal show={this.state.isOpen}
+              onClose={this.toggleModal}>
+              `Here's some content for the modal`
+            </PromptModal>
+          </div>
+    );
+  }
 }
 
 export default CreateSystem
+
+
+
+
+
+// const CreateSystem = () => {
+//     return (
+//         <div>Create System Component</div>
+//     )
+// }
+
+// export default CreateSystem
