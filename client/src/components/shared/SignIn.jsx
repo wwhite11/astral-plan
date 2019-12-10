d2c6fafb5d33b1ce09c4f2573525104bbe08bdb5
jeremyrrose <jeremy.r.rose@gmail.com>
2019-12-10 15:31:41 -0500

    one more superfluous console.log removed

--- client/src/components/shared/SignIn.jsx
+++ client/src/components/shared/SignIn.jsx
@@ -27,7 +27,6 @@ class SignIn extends Component {
         event.preventDefault()
 
         const { history, setUser } = this.props
-        console.log(history, setUser);
 
         signInUser(this.state)
         .then(res => setUser(res.user))
