909d057162cbca0f3cf7e3d9ac9ee8f4bdb53a4f
Jane Madonna <janecogger@gmail.com>
2019-12-11 10:39:16 -0500

    planet creation dynamic

--- client/src/components/shared/PlanetForm.jsx
+++ client/src/components/shared/PlanetForm.jsx
@@ -4,27 +4,29 @@ const PlanetForm = (props) => {
     const { onChange, onSubmit } = props
 	const { name, color, size } = props.formData
     return (
-        <div>
-            <div>
-                <label>Planet Name</label>
-                <input type='text' name='name' value={name} onChange={(e) => onChange(e)}/>
+        <>
+            <div className='panet-inputs'>
+                <div>
+                    <label htmlFor='name'>Planet Name</label>
+                    <input type='text' name='name' value={name} onChange={(e) => onChange(e)}/>
+                </div>
+                <div>
+                    <label htmlFor='color'>Color</label>
+                    <select name='color' value={color} onChange={(e) => onChange(e)}>
+                        <option value='red'>red</option>
+                        <option value='orange'>orange</option>
+                        <option value='yellow'>yellow</option>
+                        <option value='green'>green</option>
+                        <option value='blue'>blue</option>
+                        <option value='purple'>purple</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor='size'>Size</label>
+                    <input name='size' value={size} type='range' min='1' max='100' onChange={(e) => onChange(e)}/>
+                </div>
             </div>
-            <div>
-                <label>Color</label>
-                <select name='color' value={color} onChange={(e) => onChange(e)}>
-                    <option value='red'>red</option>
-                    <option value='orange'>orange</option>
-                    <option value='yellow'>yellow</option>
-                    <option value='green'>green</option>
-                    <option value='blue'>blue</option>
-                    <option value='purple'>purple</option>
-                </select>
-            </div>
-            <div>
-                <label>Size</label>
-                <input name='size' value={size} type='range' min='1' max='100' onChange={(e) => onChange(e)}/>
-            </div>
-        </div>
+        </>
     )
 }
 
