4a8d13bc45c3c8437d461152b1c1a0c5044f561f
Jane Madonna <janecogger@gmail.com>
2019-12-12 17:44:39 -0500

    linked systems to stars, organized dashboard

--- client/src/components/shared/UserStars.jsx
+++ client/src/components/shared/UserStars.jsx
@@ -27,17 +27,19 @@ class UserStars extends React.Component {
     render() {
         const stars = this.state.stars.map(star => {
             return (
-                <div>
+                <div className='system'>
                     <p>Star Name: {star.name}</p>
-                    <div className='star-render' style={{backgroundColor: star.color, width: parseInt(star.size), height: parseInt(star.size)}}>
-                        <img src={sunTexture} className='sun-texture' style={{width: parseInt(star.size), height: parseInt(star.size)}} alt='sun'/>
-                    </div>
-                    <div>
-                        {star.Planets.map(planet => {
-                            return (
-                                <div className='planet-render' style={{backgroundColor: planet.baseColor, width: parseInt(planet.size), height: parseInt(planet.size)}}></div>
-                            )
-                        })}
+                    <div className='star-planets'>
+                        <NavLink to={`/stars/${star.id}`}><div className='star-render' style={{backgroundColor: star.color, width: parseInt(star.size), height: parseInt(star.size)}}>
+                            <img src={sunTexture} className='sun-texture' style={{width: parseInt(star.size), height: parseInt(star.size)}} alt='sun'/>
+                        </div></NavLink>
+                        <div className='planets'>
+                            {star.Planets.map(planet => {
+                                return (
+                                    <div className='planet-render' style={{backgroundColor: planet.baseColor, width: parseInt(planet.size), height: parseInt(planet.size)}}></div>
+                                )
+                            })}
+                        </div>
                     </div>
                     <NavLink to={`/stars/${star.id}/create-planet`}><button>+ ADD PLANET</button></NavLink>
                     <Link to={`/update-system/${star.id}`}><button>:: UPDATE SYSTEM </button></Link>
--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -5,9 +5,11 @@ import UserStars from '../components/shared/UserStars'
 const Dashboard = (props) => {
     return (
         <div>
-            <h1>ASTRAL PLAN</h1>
-            <NavLink to='/create-system'><button>+ CREATE STAR</button></NavLink>
-            <h3>Your Systems</h3>
+            <h1 className='dash-header'>ASTRAL PLAN</h1>
+            <div className='username-create'>
+                <h3>{props.user.username}'s Systems</h3>
+                <NavLink to='/create-system'><button>+ CREATE STAR</button></NavLink>
+            </div>
             <UserStars user={props.user}/>
         </div>
     )
