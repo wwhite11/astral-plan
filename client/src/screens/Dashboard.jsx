9d69e1fb929a052f5f665c3ca29e680ecee190a8
Jane Madonna <janecogger@gmail.com>
2019-12-12 10:04:43 -0500

    user stars rendering

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import UserStars from '../components/shared/UserStars'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
     return (
         <div>
             <h1>ASTRAL PLAN</h1>
             <NavLink to='/create-system'><button>+ CREATE STAR</button></NavLink>
             <h3>Your Systems</h3>
-            <UserStars />
+            <UserStars user={props.user}/>
         </div>
     )
 }
